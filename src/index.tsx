688a49cf90ba662d090a12a1437454435ccfa8d3
Nurvive <jiimbo689@gmail.com>
2022-10-04 22:49:40 +0700

    add login page

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { GlobalStyles } from './globalStyles.style';
 import { Landing } from './pages/Landing';
 import './assets/style/fonts.css';
-import { LANDING } from './constants/links';
+import { LANDING, SIGN_IN } from './constants/links';
+import { Login } from './pages/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 const router = createBrowserRouter([
     {
         path: LANDING,
         element: <Landing />,
+    },
+    {
+        path: SIGN_IN,
+        element: <Login />,
     },
 ]);
 
